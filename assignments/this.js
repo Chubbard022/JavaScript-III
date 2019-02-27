0a828b06bac44d80a7cd8a63fd9e76ea37cfbbf7
chubbard022 <chubbard02@gmail.com>
2019-02-27 12:13:58 -0800

    finished four principles of this keyword

--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,22 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window Binding = binding the content of what this is. It will return the window and its properties and prototypes.
+* 2. Implicit Binding = Binding that occurs on whatever is applied to the left of the this keyword
+* 3. Explicit Binding = Binding that is for functions. it can help add prototypes and other important bindings to a function
+* 4. New Binding = Binding that occurs to a constructor function that allows you to create an instance of that constructor.
 *
 * write out a code example of each explanation above
 */
 
 // Principle 1
-
 // code example for Window Binding
 
 // Principle 2
-
 // code example for Implicit Binding
 
 // Principle 3
-
 // code example for New Binding
 
 // Principle 4
-
 // code example for Explicit Binding
